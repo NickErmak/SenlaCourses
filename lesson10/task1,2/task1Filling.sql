cfb9a1ab5b294577a0c18b5737cb14d011681445
Nikita Ermak <karmapoliceproduction@gmail.com>
2018-02-07 09:32:25 +0300

    change code style

--- lesson10/task1,2/task1Filling.sql
+++ lesson10/task1,2/task1Filling.sql
@@ -1,9 +1,10 @@
 use task10Ermak;
 
-DELETE FROM pc;
-DELETE FROM laptop;
-DELETE FROM printer;
-DELETE FROM product;
+
+delete from pc;
+delete from printer;
+delete from laptop;
+delete from product;
 
 insert into product (maker, model, type) values 
 ('SAMSUNG', 'samsung1001', 'PC'),
@@ -29,17 +30,17 @@ insert into pc (code, model, speed, ram, hd, cd, price) values
 (172612678, 'samsung1003', 4096, 2048, 1024, '24x', 650),
 (172365689, 'asus1004', 512, 1024, 256, '8x', 350),
 (172344521, 'asus1003', 1024, 2048, 1024, '24x', 480),
-(172262521, 'hp1004', 4096, 1024, 1024, '24x', 480),
-(172262231, 'brother10', 2048, 1024, 1024, '24x', 530);
+(172262521, 'hp1004', 4096, 1024, 512, '24x', 480),
+(172262231, 'brother10', 2048, 1024, 1024, '24x', 1500);
     
 insert into laptop (code, model, speed, ram, hd, price, screen) values 
 (12345643, 'lg666', 2048, 4096, 512, 900, 15),  
 (12345236, 'apple001', 4096, 8192, 1024, 1500, 18),
 (12345234, 'apple001', 4096, 8192, 1024, 1500, 18),
-(12345352, 'apple002', 512, 2028, 124, 500, 12),
+(12345352, 'apple002', 256, 2028, 124, 500, 12),
 (12378954, 'asus1008', 2048, 4096, 256, 450, 16);
 
 insert into printer (code, model, color, type, price) values 
 (1235768, 'hp00451', 'y', 'Laser', 200),    
-(5465768, 'hp00011', 'y', 'Jet', 150),
+(5465768, 'hp00011', 'y', 'Jet', 80),
 (5465131, 'brother100', 'n', 'Matrix', 80);
--- lesson10/task1,2/task2.sql
+++ lesson10/task1,2/task2.sql
@@ -1,58 +1,76 @@
 /*task 2.1
-select model, speed, hd from pc where price < 500*/
+select model, speed, hd from pc 
+where price < 500*/
 /*task 2.2
-select distinct maker from product where type = 'Printer';*/
+select distinct maker from product 
+where type = 'Printer';*/
 /*task 2.3
-select model, ram, screen from laptop where price > 1000*/
+select model, ram, screen from laptop 
+where price > 1000*/
 /*task 2.4
-SELECT * FROM printer WHERE color = 'y';*/
+SELECT * FROM printer 
+WHERE color = 'y'*/
 /*task 2.5
-select model, speed, hd from pc where cd = '12x' or cd = '24x' and price < 600;*/
+select model, speed, hd from pc 
+where cd = '12x' or cd = '24x' 
+and price < 600*/
 /*task 2.6
-select laptop.speed, product.maker from laptop inner join product on product.model = laptop.model where hd >= 10;*/
+select p.maker, l.speed from laptop l
+inner join product p on p.model = l.model 
+where hd >= 10*/
 /*task 2.7
-select product.model, price from pc join product on pc.model = product.model
-where product.maker like 'B%'
+select p.model, price from pc 
+join product p on pc.model = p.model
+where p.maker like 'B%'
 union
-select product.model, price from printer join product on printer.model = product.model
-where product.maker like 'B%'
+select p.model, price from printer 
+join product p on printer.model = p.model
+where p.maker like 'B%'
 union
-select product.model, price from laptop join product on laptop.model = product.model
-where product.maker like 'B%'*/
+select p.model, price from laptop 
+join product p on laptop.model = p.model
+where p.maker like 'B%'*/
 /*task 2.8
-select maker from product where maker not in 
-(select maker from product where type = 'Laptop')
-and type = 'pc';*/
+select distinct maker from product 
+where type = 'pc' 
+and maker not in 
+(select maker from product where type = 'Laptop')*/
 /*task 2.9
-select distinct product.maker from pc 
-join product on pc.model = product.model
-where speed > 450*/
+select distinct p.maker from pc, product p
+where pc.model = p.model
+and speed > 450*/
 /*task 2.10
 select model, price as 'max price' from printer 
-where price = (select max(price) from printer);*/
+where price = (select max(price) from printer)*/
 /*task 2.11
 select avg(speed) from pc*/
 /*task 2.12
-select avg(speed) from laptop where price > 1000*/
+select avg(speed) from laptop 
+where price > 1000*/
 /*task 2.13
-select avg(speed) from pc, product where pc.model = product.model and product.maker = 'SAMSUNG'*/
+select avg(speed) from pc, product p
+where pc.model = p.model 
+and p.maker = 'SAMSUNG'*/
 /*task 2.14
-select speed, avg(price) from pc group by speed*/
+select speed, avg(price) from pc 
+group by speed*/
 /*task 2.15
 select hd from pc 
 group by hd 
 having count(hd) > 1*/
 /*task 2.16
 select pc1.model as model1, pc2.model as model2, pc1.speed, pc1.ram from pc as pc1
-inner join pc as pc2 on pc1.ram = pc2.ram and pc1.speed = pc2.speed
+join pc as pc2 on pc1.ram = pc2.ram and pc1.speed = pc2.speed
 where pc1.code > pc2.code*/
 /*task 2.17
 select p.type, l.model, l.speed from laptop l, product p
-where l.speed < (select min(speed) from pc)
-and l.model = p.model*/
+where l.model = p.model
+and l.speed < (select min(speed) from pc)*/
 /*task 2.18
-select product.maker, min(printer.price) from printer, product
-where product.model = printer.model*/
+select p.maker, printer.price from printer, product p
+where p.model = printer.model
+and printer.price = 
+(select min(price) from printer where color = 'y')*/
 /*task 2.19
 select p.maker, avg(l.screen) from product p, laptop l
 where p.model = l.model
@@ -63,8 +81,8 @@ where type = 'pc'
 group by maker
 having count(model) > 2*/
 /*task 2.21
-select maker, max(price) from pc, product 
-where pc.model = product.model
+select maker, max(price) from pc, product p
+where pc.model = p.model
 group by maker*/
 /*task 2.22
 select speed, avg(price) from pc
@@ -80,45 +98,25 @@ where p.model = l.model
 and l.speed > 750)*/
 /*task 2.24
 select model from 
+
 (select model, max(price) as price from laptop
 union
 select model, max(price) as price from pc
 union
-select model, max(price) as price from printer) as T
-having max(price)*/
+select model, max(price) as price from printer) as maxPriceAll
+
+join (select max(price) as price from 
+(select model, max(price) as price from laptop
+union
+select model, max(price) as price from pc
+union
+select model, max(price) as price from printer) as T) as maxPriceTotal
+on maxPriceAll.price = maxPriceTotal.price*/
 /*task 2.25
-select maker, speed from pc, product
-where pc.model = product.model
-and maker in
-(select maker from product 
-where type = 'pc' 
-
-and maker in
-(select distinct maker from product
-where type = 'printer'))
-
-and ram = (select min(ram) from pc, product
-where pc.model = product.model
-and maker in
-(select maker from product 
-where type = 'pc' 
-and maker in
-(select distinct maker from product
-where type = 'printer'))) 
-
-and speed = (select max(speed) from pc, product
-where pc.model = product.model
-and maker in
-(select maker from product 
-where type = 'pc' 
-and maker in
-(select distinct maker from product
-where type = 'printer'))
-and ram = (select min(ram) from pc, product
-where pc.model = product.model
-and maker in
-(select maker from product 
-where type = 'pc' 
-and maker in
-(select distinct maker from product
-where type = 'printer'))))*/
+select maker from  product, pc
+where product.model = pc.model
+and speed = (select max(speed) from pc 
+where ram = (select min(ram) from pc))
+and maker in 
+(select  maker from product
+where type = 'printer')*/
